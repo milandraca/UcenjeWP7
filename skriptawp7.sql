7b34a3e01457c6de2623218fd63146ad7af3e7b8
Milan Draca <milan.draca@gmail.com>
2024-11-27 20:18:53 +0100

    kreiranje tablice smjerovi

--- skriptawp7.sql
+++ skriptawp7.sql
@@ -12,6 +12,14 @@ go
 
 --use edunovawp7;
 
-create table smjer(
-naziv varchar(50)
+create table smjerovi(
+sifra int,
+naziv varchar (50),
+trajanje int,
+cijena decimal(18,2),
+vaucer bit,
+izvodiseod datetime
 );
+
+
+
