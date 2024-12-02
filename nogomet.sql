99e90f55c4439d349ca936a901e48af311b0f855
Milan Draca <milan.draca@gmail.com>
2024-12-02 20:13:27 +0100

    Nogomet

---
+++ nogomet.sql
@@ -0,0 +1,47 @@
+use master;
+go
+drop database if exists nogomet;
+go
+create database nogomet;
+go
+use nogomet;
+go
+
+
+create table klubovi(
+sifra int not null primary key identity(1,1),
+naziv varchar(50) not null,
+osnovan date,
+stadion varchar not null,
+drzava varchar not null,
+liga varchar not null
+);
+
+create table igraci(
+sifra int not null ,
+ime varchar(50) not null,
+prezime varchar(50) not null,
+datumrodjenja date not null,
+pozicija varchar ,
+brojdresa varchar not null,
+klub int not null references klubovi(sifra)
+);
+
+create table treneri(
+sifra int not null,
+ime varchar(50) not null,
+prezime varchar(50) not null,
+klub int not null references klubovi(sifra),
+nacionalnost varchar ,
+iskustvogodina int
+);
+
+create table utakmice(
+sifra int not null references klubovi(sifra),
+datum date not null,
+vrijeme time not null,
+lokacija varchar,
+stadion varchar ,
+domaci_klub int not null references klubovi(sifra),
+gostujuci_klub int not null references klubovi(sifra),
+);
