46d2729b81f284f821525bd95b80623e44d5e0d5
milandraca <38816321+milandraca@users.noreply.github.com>
2024-12-05 19:06:21 +0100

    Insert

--- weatherstation.sql
+++ weatherstation.sql
@@ -1,4 +1,4 @@
-use master;
+﻿use master;
 go
 
 drop database if exists weatherstation;
@@ -16,25 +16,59 @@ naziv varchar(50) not null
 
 create table regija(
 sifra int not null primary key identity(1,1),
-naziv int not null references drzava(sifra),
+naziv varchar (50) not null ,
 drzava int not null references drzava(sifra)
 );
 
 create table mjesto(
 sifra int not null primary key identity(1,1),
 naziv varchar(30) not null,
-brojposte int not null ,
-regija varchar (50)
+brojposte int,
+regija int  not null references regija(sifra)
 );
 
 create table meteostanica( 
-sifra int not null,
-naziv varchar (50),
+sifra int not null primary key identity(1,1),
+naziv varchar (50) not null,
 brzinavjetra int,
 longitude DECIMAL(9,6),
+latitude DECIMAL(9,6),
 temperatura decimal(3,1) ,
 relativnavlaga decimal(4,2),
 kolicinapadalinadan decimal(4,2),
 kolicinapadalinamjesec decimal(5,2),
-mjesto int references mjesto(sifra)
+mjesto int not null references mjesto(sifra)
 );
+
+
+-- 1 (Ovo je šifra koju je dodjelila baza)
+insert into drzava 
+(naziv) values
+('Hrvatska');
+
+insert into drzava(naziv) values
+-- 2
+('Srbija');
+
+
+
+insert into regija (naziv, drzava) values
+-- 1
+('Osječko-baranjska županija',1 ),
+-- 2
+('Vojvodina',2 );
+
+insert into mjesto(naziv, brojposte, regija) values
+('Jagodnjak',null,1 ),
+('Novi Sad',null, 2);
+
+
+insert into meteostanica (naziv, brzinavjetra, longitude, latitude, temperatura, relativnavlaga, kolicinapadalinadan, kolicinapadalinamjesec, mjesto) 
+values 
+('GW1101', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1),
+('WH2650A', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 2),
+('GW2001', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1);
+
+
+select * from meteostanica;
+
